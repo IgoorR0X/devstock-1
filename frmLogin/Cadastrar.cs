4cbb04cb43b6b45be8e34a3ea36cab7494054cd3
Marcos Tesolin Munhoz <marmunhoz414@gmail.com>
2018-10-09 23:29:55 -0300

    primeiro commit

---
+++ frmLogin/Cadastrar.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace frmLogin
+{
+    public class Cadastrar
+    {
+        ConnectDB conectar = new ConnectDB();
+        SqlCommand cmd = new SqlCommand();
+        public String mensagem;
+
+        public Cadastrar(String usuario, String senha){
+
+            //Comando SQL
+            cmd.CommandText = "select * from usuarios where usuario = @usuario and senha = @senha";
+
+            //parametros
+            cmd.Parameters.AddWithValue("@usuario", usuario);
+            cmd.Parameters.AddWithValue("@senha", senha);
+
+            try
+            {
+            //conectar banco de dados
+                cmd.Connection = conectar.conexao();
+            //executar comando
+                cmd.ExecuteNonQuery();
+            //desconectar banco de dados
+                conectar.desconectar();
+            //Mensagem de sucesso
+                this.mensagem = "Logado com sucesso";
+            }
+
+            catch (SqlException e)
+            {
+                this.mensagem = "Erro ao se conectar no banco de dados.";
+            }
+        }
+    }
+}
---
+++ frmLogin/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace frmLogin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbLogin = new System.Windows.Forms.TextBox();
+            this.txbPassw = new System.Windows.Forms.TextBox();
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Senha:";
+            // 
+            // txbLogin
+            // 
+            this.txbLogin.Location = new System.Drawing.Point(102, 66);
+            this.txbLogin.Name = "txbLogin";
+            this.txbLogin.Size = new System.Drawing.Size(116, 20);
+            this.txbLogin.TabIndex = 2;
+            // 
+            // txbPassw
+            // 
+            this.txbPassw.Location = new System.Drawing.Point(102, 103);
+            this.txbPassw.Name = "txbPassw";
+            this.txbPassw.PasswordChar = '*';
+            this.txbPassw.Size = new System.Drawing.Size(116, 20);
+            this.txbPassw.TabIndex = 3;
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.Location = new System.Drawing.Point(61, 153);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(75, 23);
+            this.btnEntrar.TabIndex = 4;
+            this.btnEntrar.Text = "Entrar";
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            this.btnEntrar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(143, 153);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 5;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 235);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnEntrar);
+            this.Controls.Add(this.txbPassw);
+            this.Controls.Add(this.txbLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbLogin;
+        private System.Windows.Forms.TextBox txbPassw;
+        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
